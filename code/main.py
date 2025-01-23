ed7dc39e8332822085482b208e7b826ba4cd7fc6
floraprvt <f.preuvot@live.fr>
2025-01-23 16:29:23 -0500

    add player + bg + collisions + camera

--- code/main.py
+++ code/main.py
@@ -1,16 +1,35 @@
 from settings import * 
+from sprites import *
+from groups import *
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Platformer')
+        pygame.display.set_caption('Platgaetformer')
         self.clock = pygame.time.Clock()
         self.running = True
 
         # groups 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+
+    # sprites
+    def setup(self):
+        map = load_pygame(join("data", "maps", "world.tmx"))
+
+        for x, y, image in map.get_layer_by_name("Main").tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, (self.all_sprites, self.collision_sprites))
+
+        for x, y, image in map.get_layer_by_name("Decoration").tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name("Entities"):
+            if obj.name == "Player":
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
 
     def run(self):
@@ -26,7 +45,7 @@ class Game:
 
             # draw 
             self.display_surface.fill(BG_COLOR)
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
         
         pygame.quit()
