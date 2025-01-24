4b353e4edf388e9fbc988c0cf2fe067b6f99b7b5
floraprvt <f.preuvot@live.fr>
2025-01-24 10:00:23 -0500

    add load images

--- code/main.py
+++ code/main.py
@@ -1,55 +1,72 @@
-from settings import * 
+from settings import *
 from sprites import *
 from groups import *
+from support import *
+
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Platgaetformer')
+        pygame.display.set_caption("Platgaetformer")
         self.clock = pygame.time.Clock()
         self.running = True
 
-        # groups 
+        # groups
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
 
+    def load_assets(self):
+        # graphics
+        self.player_frames = import_folder("images", "player")
+        self.bullet_surf = import_image("images", "gun", "bullet")
+        self.fire_surf = import_image("images", "gun", "fire")
+        self.bee_frames = import_folder("images", "enemies", "bee")
+        self.worm_frames = import_folder("images", "enemies", "worm")
+
+        # sounds
 
     # sprites
     def setup(self):
         map = load_pygame(join("data", "maps", "world.tmx"))
 
         for x, y, image in map.get_layer_by_name("Main").tiles():
-            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, (self.all_sprites, self.collision_sprites))
+            Sprite(
+                (x * TILE_SIZE, y * TILE_SIZE),
+                image,
+                (self.all_sprites, self.collision_sprites),
+            )
 
         for x, y, image in map.get_layer_by_name("Decoration").tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
 
         for obj in map.get_layer_by_name("Entities"):
             if obj.name == "Player":
-                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
-
+                self.player = Player(
+                    (obj.x, obj.y), self.all_sprites, self.collision_sprites
+                )
 
     def run(self):
         while self.running:
-            dt = self.clock.tick(FRAMERATE) / 1000 
+            dt = self.clock.tick(FRAMERATE) / 1000
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.running = False 
-            
+                    self.running = False
+
             # update
             self.all_sprites.update(dt)
 
-            # draw 
+            # draw
             self.display_surface.fill(BG_COLOR)
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
-        
+
         pygame.quit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     game = Game()
-    game.run() 
+    game.run()
